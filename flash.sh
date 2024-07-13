f82dc50705c473c7bc821a3d93190963713bd25e
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-07-13 19:56:58 -0400

    Linux + bootloader in 512KB of ROM

--- flash.sh
+++ flash.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-
-PORT=/dev/ttyACM0
-
-ls -lah images
-
-echo "Copying Mackerel image..."
-echo "load 100000" > $PORT
-sleep 1
-ctt -p $PORT -f images/mackerel.rom
-
-sleep 1
-echo "Booting Linux..."
-echo "runrom" > $PORT
-
-echo "Done"
+minipro -p SST39SF040 -s -w images/mackerel.bin
