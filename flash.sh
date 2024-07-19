cdc9cbff1d480255ac71084be0fcdb86b49c37ea
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-07-19 09:14:05 -0400

    Good config  running from RAM - vi, Advent4, ~440k

--- flash.sh
+++ flash.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-minipro -p SST39SF040 -s -w images/mackerel.bin
+
+PORT=/dev/ttyUSB0
+
+ls -lah images
+
+echo "Copying Mackerel image..."
+echo "load 300000" > $PORT
+sleep 1
+ctt -p $PORT -f images/linux.bin
+
+sleep 1
+echo "Booting Linux..."
+echo "runrom" > $PORT
+
+echo "Done"
